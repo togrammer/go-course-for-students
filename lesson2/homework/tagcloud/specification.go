62339220089b6815cd8c0f5ad02a8ab549c633ad
togrammer <toprogrammer@yandex.ru>
2023-03-05 03:55:32 +0300

    back

--- lesson2/homework/tagcloud/specification.go
+++ lesson2/homework/tagcloud/specification.go
@@ -1,10 +1,8 @@
 package tagcloud
 
-import "math"
-
 // TagCloud aggregates statistics about used tags
 type TagCloud struct {
-	StatSlice []TagStat
+	// TODO: add fields if necessary
 }
 
 // TagStat represents statistics regarding single tag
@@ -14,27 +12,17 @@ type TagStat struct {
 }
 
 // New should create a valid TagCloud instance
+// TODO: You decide whether this function should return a pointer or a value
 func New() TagCloud {
-	OurSlice := []TagStat{}
-	return TagCloud{StatSlice: OurSlice}
+	// TODO: Implement this
+	return TagCloud{}
 }
 
 // AddTag should add a tag to the cloud if it wasn't present and increase tag occurrence count
 // thread-safety is not needed
-func (cloud *TagCloud) AddTag(tag string) {
-	for i, v := range cloud.StatSlice {
-		if v.Tag == tag {
-			cloud.StatSlice[i].OccurrenceCount++
-			for j := i - 1; j >= 0; j-- {
-				if cloud.StatSlice[j].OccurrenceCount < cloud.StatSlice[j+1].OccurrenceCount {
-					cloud.StatSlice[j], cloud.StatSlice[j+1] = cloud.StatSlice[j+1], cloud.StatSlice[j]
-				}
-			}
-			return
-		}
-	}
-
-	cloud.StatSlice = append(cloud.StatSlice, TagStat{Tag: tag, OccurrenceCount: 1})
+// TODO: You decide whether receiver should be a pointer or a value
+func (TagCloud) AddTag(tag string) {
+	// TODO: Implement this
 }
 
 // TopN should return top N most frequent tags ordered in descending order by occurrence count
@@ -42,7 +30,8 @@ func (cloud *TagCloud) AddTag(tag string) {
 // if n is greater that TagCloud size then all elements should be returned
 // thread-safety is not needed
 // there are no restrictions on time complexity
-func (cloud *TagCloud) TopN(n int) []TagStat {
+// TODO: You decide whether receiver should be a pointer or a value
+func (TagCloud) TopN(n int) []TagStat {
 	// TODO: Implement this
-	return cloud.StatSlice[0:int(math.Min(float64(n), float64(len(cloud.StatSlice))))]
+	return nil
 }
