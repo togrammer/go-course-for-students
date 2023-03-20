2235a4a6e365c3a5b8f35af06ee257ca6c47a975
togrammer <toprogrammer@yandex.ru>
2023-03-20 23:46:48 +0300

    new hw

--- lesson4/homework/storage/dir_sizer.go
+++ lesson4/homework/storage/dir_sizer.go
@@ -2,6 +2,7 @@ package storage
 
 import (
 	"context"
+	"sync"
 )
 
 // Result represents the Size function result
@@ -20,18 +21,69 @@ type DirSizer interface {
 
 // sizer implement the DirSizer interface
 type sizer struct {
-	// maxWorkersCount number of workers for asynchronous run
-	maxWorkersCount int
-
-	// TODO: add other fields as you wish
+	maxWorkersCount int // number of workers for asynchronous run
 }
 
 // NewSizer returns new DirSizer instance
 func NewSizer() DirSizer {
-	return &sizer{}
+	return &sizer{maxWorkersCount: 10}
 }
 
 func (a *sizer) Size(ctx context.Context, d Dir) (Result, error) {
-	// TODO: implement this
-	return Result{}, nil
+	result := Result{}
+	resultChan := make(chan Result)
+	semaphoreChan := make(chan struct{}, a.maxWorkersCount)
+	defer close(resultChan)
+	defer close(semaphoreChan)
+	var totalError error = nil
+	dirs, files, err := d.Ls(ctx)
+	if err != nil {
+		return result, err
+	}
+	var wg sync.WaitGroup
+	wg.Add(len(dirs) + len(files))
+
+	for _, file := range files {
+		semaphoreChan <- struct{}{}
+		go func(file File) {
+			defer func() {
+				<-semaphoreChan
+				wg.Done()
+			}()
+			size, err := file.Stat(ctx)
+			if err != nil {
+				totalError = err
+				return
+			}
+			result.Size += size
+			result.Count++
+		}(file)
+	}
+
+	for _, dir := range dirs {
+		semaphoreChan <- struct{}{}
+		go func(dir Dir) {
+			defer func() {
+				<-semaphoreChan
+				wg.Done()
+			}()
+			dirResult, err := a.Size(ctx, dir)
+			if err != nil {
+				totalError = err
+			}
+			resultChan <- dirResult
+		}(dir)
+	}
+
+	for i := 0; i < len(dirs); i++ {
+		select {
+		case <-ctx.Done():
+			return result, totalError
+		case dirResult := <-resultChan:
+			result.Size += dirResult.Size
+			result.Count += dirResult.Count
+		}
+	}
+	wg.Wait()
+	return result, totalError
 }
