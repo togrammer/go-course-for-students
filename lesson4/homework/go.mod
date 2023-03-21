33846198a3db23cc9b42425e1cb9d941f02b53a1
togrammer <toprogrammer@yandex.ru>
2023-03-21 15:05:49 +0300

    t

--- lesson4/homework/go.mod
+++ lesson4/homework/go.mod
@@ -5,6 +5,7 @@ go 1.19
 require (
 	github.com/golang/mock v1.6.0
 	github.com/stretchr/testify v1.8.2
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
 )
 
 require (
--- lesson4/homework/storage/dir_sizer.go
+++ lesson4/homework/storage/dir_sizer.go
@@ -2,8 +2,6 @@ package storage
 
 import (
 	"context"
-	"sync"
-	"sync/atomic"
 )
 
 // Result represents the Size function result
@@ -22,70 +20,18 @@ type DirSizer interface {
 
 // sizer implement the DirSizer interface
 type sizer struct {
-	maxWorkersCount int // number of workers for asynchronous run
+	// maxWorkersCount number of workers for asynchronous run
+	maxWorkersCount int
+
+	// TODO: add other fields as you wish
 }
 
 // NewSizer returns new DirSizer instance
 func NewSizer() DirSizer {
-	return &sizer{maxWorkersCount: 10}
+	return &sizer{}
 }
 
 func (a *sizer) Size(ctx context.Context, d Dir) (Result, error) {
-	result := Result{}
-	resultChan := make(chan Result)
-	semaphoreChan := make(chan struct{}, a.maxWorkersCount)
-	defer close(resultChan)
-	defer close(semaphoreChan)
-
-	var totalError error = nil
-	dirs, files, err := d.Ls(ctx)
-	if err != nil {
-		return result, err
-	}
-	var wg sync.WaitGroup
-	wg.Add(len(dirs) + len(files))
-
-	for _, file := range files {
-		semaphoreChan <- struct{}{}
-		go func(file File) {
-			defer func() {
-				<-semaphoreChan
-				wg.Done()
-			}()
-			size, err := file.Stat(ctx)
-			if err != nil {
-				totalError = err
-				return
-			}
-			atomic.AddInt64(&result.Size, size)
-			atomic.AddInt64(&result.Count, 1)
-		}(file)
-	}
-
-	for _, dir := range dirs {
-		semaphoreChan <- struct{}{}
-		go func(dir Dir) {
-			defer func() {
-				<-semaphoreChan
-				wg.Done()
-			}()
-			dirResult, err := a.Size(ctx, dir)
-			if err != nil {
-				totalError = err
-			}
-			resultChan <- dirResult
-		}(dir)
-	}
-
-	for i := 0; i < len(dirs); i++ {
-		select {
-		case <-ctx.Done():
-			return result, totalError
-		case dirResult := <-resultChan:
-			atomic.AddInt64(&result.Size, dirResult.Size)
-			atomic.AddInt64(&result.Count, dirResult.Count)
-		}
-	}
-	wg.Wait()
-	return result, totalError
+	// TODO: implement this
+	return Result{}, nil
 }
