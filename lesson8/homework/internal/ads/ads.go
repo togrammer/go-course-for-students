f8d6eae5cb091dbabee105a1d32d12e94df82771
togrammer <toprogrammer@yandex.ru>
2023-04-18 18:17:21 +0300

    hw8

--- lesson8/homework/internal/ads/ads.go
+++ lesson8/homework/internal/ads/ads.go
@@ -1,9 +1,13 @@
 package ads
 
-type Ad struct {
+import "time"
+
+type Ad struct { //добавить в структуру объявления дату создания, обновления объявления и поддержать ее в существующих методах
 	ID        int64
-	Title     string
-	Text      string
+	Title     string `validate:"range:1,99"`
+	Text      string `validate:"range:1,499"`
 	AuthorID  int64
 	Published bool
+	Created   time.Time
+	Updated   time.Time
 }
