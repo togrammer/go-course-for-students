f55cd6d463617a4ec5a685120932cea10283ff02
togrammer <toprogrammer@yandex.ru>
2023-04-27 01:09:04 +0300

    fix

--- lesson8/homework/internal/adapters/userrepo/userRepo.go
+++ lesson8/homework/internal/adapters/userrepo/userRepo.go
@@ -5,14 +5,18 @@ import (
 	"errors"
 	"homework8/internal/app"
 	"homework8/internal/users"
+	"sync"
 )
 
 type Users struct {
 	lastId   int64
 	userList []users.User
+	mtx      *sync.RWMutex
 }
 
 func (u Users) FindUser(ctx context.Context, userID int64) (users.User, error) {
+	u.mtx.RLock()
+	defer u.mtx.RUnlock()
 	if userID >= u.lastId {
 		return users.User{}, errors.New("wrong userId")
 	}
@@ -20,18 +24,22 @@ func (u Users) FindUser(ctx context.Context, userID int64) (users.User, error) {
 }
 
 func (u Users) AddUser(ctx context.Context, nickname, email string) users.User {
+	u.mtx.Lock()
 	defer func() {
 		u.lastId++
+		u.mtx.Unlock()
 	}()
 	u.userList = append(u.userList, users.User{ID: u.lastId, Nickname: nickname, Email: email})
 	return u.userList[u.lastId]
 }
 
 func (u Users) UpdateUser(ctx context.Context, userID int64, nickname, email string) {
+	u.mtx.Lock()
+	defer u.mtx.Unlock()
 	u.userList[userID].Nickname = nickname
 	u.userList[userID].Email = email
 }
 
 func New() app.UsersRepository {
-	return &Users{}
+	return &Users{mtx: &sync.RWMutex{}}
 }
